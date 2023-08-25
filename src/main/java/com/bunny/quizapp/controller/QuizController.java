4b8b85f8dab117e59ad2b31b4aaed28282420673
aniket <aniket.umbc@gmail.com>
2023-08-25 07:33:39 +0530

    submit quiz method added

--- src/main/java/com/bunny/quizapp/controller/QuizController.java
+++ src/main/java/com/bunny/quizapp/controller/QuizController.java
@@ -7,33 +7,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.bunny.quizapp.model.QuestionWrapper;
+import com.bunny.quizapp.model.Response;
 import com.bunny.quizapp.service.QuizService;
 
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
-	
+
 	@Autowired
 	QuizService quizService;
-	
+
 	@PostMapping("create")
-	public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
-		
-		return quizService.createQuiz(category,numQ,title);
+	public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ,
+			@RequestParam String title) {
+		return quizService.createQuiz(category, numQ, title);
 	}
-	
-	
-	
+
 	@GetMapping("get/{id}")
-	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
 		return quizService.getQuizQuestions(id);
-		
 	}
-	
-	
+
+	@PostMapping("Submit/{id}")
+	public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
+		return quizService.calculateResult(id, responses);
+	}
 
 }
--- src/main/java/com/bunny/quizapp/service/QuizService.java
+++ src/main/java/com/bunny/quizapp/service/QuizService.java
@@ -14,48 +14,64 @@ import com.bunny.quizapp.doa.QuizDao;
 import com.bunny.quizapp.model.Question;
 import com.bunny.quizapp.model.QuestionWrapper;
 import com.bunny.quizapp.model.Quiz;
+import com.bunny.quizapp.model.Response;
 
 @Service
 public class QuizService {
-	
-	
+
 	@Autowired
 	QuizDao quizDao;
-	
+
 	@Autowired
 	QuestionDao questionDao;
 
 	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-		
-		
-		List<Question> questions = questionDao.findRandomQuestionByCategory(category,numQ);
-		
+
+		List<Question> questions = questionDao.findRandomQuestionByCategory(category, numQ);
+
 		Quiz quiz = new Quiz();
-		
+
 		quiz.setTitle(title);
 		quiz.setQuestions(questions);
 		quizDao.save(quiz);
-		
-		
+
 		return new ResponseEntity<>("Successfully create quiz", HttpStatus.CREATED);
-		
+
 	}
 
 	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
 		Optional<Quiz> quiz = quizDao.findById(id);
 		List<Question> questionFromDb = quiz.get().getQuestions();
 		List<QuestionWrapper> questionsForUsers = new ArrayList<>();
-		
-		for(Question q: questionFromDb) {
-			QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4(),q.getLevel(),q.getQuestionTitle());
-			
+
+		for (Question q : questionFromDb) {
+			QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getOption1(), q.getOption2(), q.getOption3(),
+					q.getOption4(), q.getLevel(), q.getQuestionTitle());
+
 			questionsForUsers.add(qw);
 		}
-		
+
 		return new ResponseEntity<>(questionsForUsers, HttpStatus.OK);
 	}
-	
-	
-	
+
+	public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+
+		Quiz quiz = quizDao.findById(id).get();
+		List<Question> questions = quiz.getQuestions();
+		int right = 0;
+		int i = 0;
+
+		for (Response resp : responses) {
+
+			if (resp.getResponse().equals(questions.get(i).getAnswer())) {
+				right++;
+				
+			}
+			i++;
+		}
+
+		return new ResponseEntity<>(right, HttpStatus.OK);
+
+	}
 
 }
